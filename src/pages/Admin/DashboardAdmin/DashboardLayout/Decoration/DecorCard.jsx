f3022f36957b8e9aafe464e868693b553247fcca
mr-exploit <raffiaj8@gmail.com>
2024-06-23 23:10:48 +0700

    update data

--- src/pages/Admin/DashboardAdmin/DashboardLayout/Decoration/DecorCard.jsx
+++ src/pages/Admin/DashboardAdmin/DashboardLayout/Decoration/DecorCard.jsx
@@ -1,29 +1,34 @@
-import { Checkbox } from 'flowbite-react'
-import React from 'react'
+import { Checkbox } from 'flowbite-react';
+import React from 'react';
+
 const DecorCard = (props) => {
-    const { name, konsep, url, vendor, phone} = props
+    const { name, konsep, url, vendor, phone } = props;
+    const imageUrl = `${import.meta.env.VITE_API_URL}/${url.replace(/\\/g, '/')}`;
+
     return (
-        <>
         <div className="bg-gray-100 w-[600px] h-[450px] border mt-5">
-            <div className='flex w-full' style={{
-                backgroundImage: `url(${url})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '300px',
-            }}>
-                
-            </div>
+            <div
+                className="flex w-full"
+                style={{
+                    backgroundImage: `url(${imageUrl})`, // Correctly use `url()` for background image
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '300px',
+                }}
+            ></div>
             <div className="flex flex-row justify-between">
-                <h1 className='font-switzer font-bold m-3'>{name}</h1>
+                <h1 className="font-switzer font-bold m-3">{name}</h1>
             </div>
             <div>
-                <p className='font-switzer mx-3 mb-1 text-[14px]'>Konsep : {konsep}</p>
-                <p className='font-switzer mx-3 mb-1 text-[14px]'>Vendor {vendor}</p>
-                <p className='font-switzer mx-3 mb-1 text-[14px]'>{phone}</p>
+                <p className="font-switzer mx-3 mb-1 text-[14px]">Konsep: {konsep}</p>
+                <p className="font-switzer mx-3 mb-1 text-[14px]">Vendor: {vendor}</p>
+                <p className="font-switzer mx-3 mb-1 text-[14px]">Phone: {phone}</p>
             </div>
-            <div className='flex justify-end mr-2'>
-                <Checkbox className='bg-white border-black cursor-pointer' value=''></Checkbox>
+            <div className="flex justify-end mr-2">
+                <Checkbox className="bg-white border-black cursor-pointer" value="" />
             </div>
         </div>
-        </>
-    )
-}
+    );
+};
 
-export default DecorCard
+export default DecorCard;
--- src/pages/Admin/DashboardAdmin/DashboardLayout/Decoration/DecorMenu.jsx
+++ src/pages/Admin/DashboardAdmin/DashboardLayout/Decoration/DecorMenu.jsx
@@ -1,66 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddDecorButton from './AddDecorButton';
 import DecorCard from './DecorCard';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const DecorMenu = () => {
-    const handleAddDecor = () => {
-        // Logic untuk menambah vendor baru
-    };
-
-    const cards = [
-        {
-            url: "../src/assets/images/altar-1.png",
-            name: "Altar 1",
-            konsep: "Elegant",
-            vendor: "xxxxx",
-            phone: "088xxxxx"
-        },
-        {
-            url: "../src/assets/images/altar-2.png",
-            name: "Altar 2",
-            konsep: "Aesthetic",
-            vendor: "xxxxx",
-            phone: "088xxxxx"
-        },
-        {
-            url: "../src/assets/images/meja-tamu.png",
-            name: "Meja Tamu",
-            konsep: "Elegant",
-            vendor: "xxxxx",
-            phone: "088xxxxx"
-        },
-        {
-            url: "../src/assets/images/meja-konsumsi.png",
-            name: "Meja Konsumsi",
-            konsep: "Rustik",
-            vendor: "xxxxx",
-            phone: "088xxxxx"
-        },
-        {
-            url: "../src/assets/images/meja-tamu.png",
-            name: "Meja Tamu",
-            konsep: "Elegant",
-            vendor: "xxxxx",
-            phone: "088xxxxx"
-        },
-        {
-            url: "../src/assets/images/meja-konsumsi.png",
-            name: "Meja Konsumsi",
-            konsep: "Rustik",
-            vendor: "xxxxx",
-            phone: "088xxxxx"
-        },
-        // Add more cards here as needed
-    ];
-    
+    const [dataInterior, setDataInterior] = useState([]);
     const [visibleCount, setVisibleCount] = useState(4);
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+    const urlApiENV = import.meta.env.VITE_API_URL;
+    const url = `${urlApiENV}/api/interior`;
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/login');
+            return;
+        }
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                console.log("Fetched data", response.data.data);
+                setDataInterior(response.data.data);
+            } catch (err) {
+                console.error("Error during fetch:", err.response ? err.response.data : err.message);
+                Swal.fire({
+                    title: "Error!",
+                    text: "Data tidak ditemukan!",
+                    icon: "error",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        };
+
+        fetchData();
+    }, [navigate, token, url]);
+
+    const handleAddDecor = () => {
+        // Logic untuk menambah dekorasi baru
+    };
 
     const handleShowMore = () => {
         setVisibleCount(prevCount => prevCount + 4);
     };
 
     return (
-        <>
         <div className="p-6 bg-white border rounded-lg shadow-md m-3">
             <h1 className="text-[36px] font-switzer m-5">Interior & Decoration</h1>
             <div className="flex space-x-2 m-5">
@@ -69,18 +60,18 @@ const DecorMenu = () => {
             <div className="flex justify-center">
                 <div className="flex flex-col mb-4">
                     <div className='grid grid-cols-2 gap-14'>
-                    {cards.slice(0, visibleCount).map((card, index) => (
+                        {dataInterior.slice(0, visibleCount).map((decor, index) => (
                             <DecorCard
                                 key={index}
-                                url={card.url}
-                                name={card.name}
-                                konsep={card.konsep}
-                                vendor={card.vendor}
-                                phone={card.phone}
+                                url={decor.image}  
+                                name={decor.name}
+                                konsep={decor.konsep}
+                                vendor={decor.vendor}
+                                phone={decor.phone}
                             />
                         ))}
                     </div>
-                    {visibleCount < cards.length && (
+                    {visibleCount < dataInterior.length && (
                         <div className="flex justify-center m-10">
                             <button
                                 onClick={handleShowMore}
@@ -92,9 +83,7 @@ const DecorMenu = () => {
                     )}
                 </div>
             </div>
-            
         </div>
-        </>
     );
 };
 
